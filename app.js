e47b9d64ed94468a6562b0c85b59b9542bee3893
michaelalbinson <mikealbinson@gmail.com>
2021-11-13 13:35:10 -0800

    Update dependencies, add helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const { serverPort } = require('./config/properties.json');
 
@@ -17,6 +18,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes')(app);
